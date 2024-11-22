6ba1ce5ffce86f23e9ad3d5a25c89d3f282ef6a6
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-11-22 09:55:23 +0100

    Classe Officina con metodo ripara e aggiornamento del main.

--- src/Officina.java
+++ src/Officina.java
@@ -1,3 +1,15 @@
 public class Officina{
-
+    public double ripara(Veicolo riparazione){
+        double prezzo=0;
+        if(riparazione.getGuasto()){
+            riparazione.setGuasto(false);
+            if(riparazione instanceof Vettura){
+                prezzo=5000;
+            }
+            else if(riparazione instanceof Motociclo){
+                prezzo=2000;
+            }
+        }
+        return prezzo;
+    }
 }
--- src/UsaVeicoli.java
+++ src/UsaVeicoli.java
@@ -1,12 +1,15 @@
 public class UsaVeicoli {
     public static void main(String[]args){
-        Vettura auto1=new Vettura("Gran Turismo","GN108WB","Porsche","911 Carrera",false);
+        Officina officina=new Officina();
+        Vettura auto1=new Vettura("Gran Turismo","GN108WB","Porsche","911 Carrera",true);
         Vettura auto2=new Vettura("Utilitaria","AS246CG","Fiat","Panda",true);
-        Motociclo moto1=new Motociclo(125,"DE71464","Piaggio","Vespa Primavera",false);
+        Motociclo moto1=new Motociclo(125,"DE71464","Piaggio","Vespa Primavera",true);
         Veicolo[]veicoli={auto1,auto2,moto1};
+        System.out.println("Il veicolo riparato è targato: "+veicoli[1].getTarga()+", prezzo riparazione: "+officina.ripara(auto2));
+        System.out.println("Il veicolo riparato è targato: "+veicoli[2].getTarga()+", prezzo riparazione: "+officina.ripara(moto1));
         for(int i=0;i< veicoli.length;i++){
             if(veicoli[i].getGuasto()){
-                System.out.println(veicoli[i].getTarga());
+                System.out.println("Veicolo ancora guasto da riparare: "+veicoli[i].getTarga()+ ", costo di riparazione: "+officina.ripara(auto1));
             }
         }
     }
